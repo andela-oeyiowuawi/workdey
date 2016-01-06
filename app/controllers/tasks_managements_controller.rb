dfa8f357cc9626f85f0c00e8e3a8e8cf225b1be1
Olaide Ojewale <ojewaleolaide@gmail.com>
2016-01-06 10:38:49 +0100

    Implement the profile settings page on dashboard
     Why:
       Users have no profile settings page on the app
       This change addresses the need by:
       Creating a profile settings page
       Creating a profile update page.
       Creating a PORO for profile update and an action in the dashboard controller.
       Keeping all images on cloudinary.
       Adding feature test to the profile update branch.
    [Finished #107901600]

---
+++ app/controllers/tasks_managements_controller.rb
@@ -0,0 +1,37 @@
+class TasksManagementsController < ApplicationController
+  def index
+    @tasks = current_user.tasks_created
+  end
+
+  def show
+  end
+
+  def update
+    id = params[:task_id]
+    if TaskManagement.update(id, status: "done")
+      if review_and_rate(params)
+        flash[:notice] = "Task is completed and your reveiw has been recorded."
+      else
+        flash[:notice] = "Task is completed."
+      end
+    else
+      flash[:alert] = "Operation failed."
+    end
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def review_and_rate(params)
+    review = Review.new
+    review.rating = params[:rating] if params[:rating]
+    review.review = params[:comment] if params[:comment]
+    return "success" if review.save
+  end
+
+  def general_rating(user_id)
+    rating = 0
+    Review.where(user_id: user_id).select { |user| rating += user.rating }
+    rating
+  end
+end
