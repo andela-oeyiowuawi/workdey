d7f38f34c04323e08ff6a67d35c028a889d9de18
Olaide ojewale <ojewaleolaide@gmail.com>
2016-01-26 23:35:47 +0100

    Add other user's review to taskee profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :skillsets
   has_many :reviews
+  has_many :reviewers, class_name: "Review", foreign_key: :reviewer_id
   has_many :tasks, through: :skillsets
   has_many :tasks_given, class_name: "TaskManagement", foreign_key: :taskee_id
   has_many :tasks_created, class_name: "TaskManagement", foreign_key: :tasker_id
